dbd498f6ccb91fd9b863623f187cf50f3a473078
Thomas <tolson9@gatech.edu>
2021-01-18 21:23:18 -0500

    Added basic views for phone signin

---
+++ lib/views/verificationCode.dart
@@ -0,0 +1,56 @@
+import 'package:boilerplate/widgets/widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class VerificationCode extends StatefulWidget {
+  @override
+  _VerificationCodeState createState() => _VerificationCodeState();
+}
+
+class _VerificationCodeState extends State<VerificationCode> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: Column(
+          children: [
+            TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(12),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                hintText: "Enter Verification Code"),
+                inputFormatters:  [ LengthLimitingTextInputFormatter(10),
+                                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                                  ],
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height:40),
+            Container( 
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(vertical:20),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(30)           ),
+              child: Text("Verify", style: TextStyle(fontSize: 16, color: Colors.white)),
+            ),
+            SizedBox(height:10),
+            Container( 
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.center,
+              padding: EdgeInsets.symmetric(vertical:20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.blue),
+                borderRadius: BorderRadius.circular(30)           ),
+              child: Text("Back", style: TextStyle(fontSize: 16, color: Colors.blue)),
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
